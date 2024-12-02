92d649f1e65d3cc6e1889c724a786a57a0417669
Baley Eccles <baleycod@gmail.com>
2024-12-02 21:12:55 +1100

    Clean up day 1

--- src/main.hs
+++ src/main.hs
@@ -9,21 +9,16 @@ main = do
   contents <- hGetContents handle
   let linesOfFile = lines contents
       (leftList, rightList) = unzip $ map (toPair . words) linesOfFile
-  --print leftList
-  --print rightList
-
+      
   -- For testing 
   --let leftList = [3, 4, 2, 1, 3, 3]
   --let rightList = [4, 3, 5, 3, 9, 3]
   -- Part 1
-
                     
   let sortedLeft = sort leftList
   let sortedRight = sort rightList
-  --print sortedLeft
-  --print sortedRight
   let listDifference = [abs(x - y) | (x, y) <- zip sortedLeft sortedRight]
-  --print listDifference
+
   let sumList = sum listDifference
   print "Solution to part 1:" 
   print sumList
@@ -32,8 +27,6 @@ main = do
   let groupedLeft = group sortedLeft
   let groupedRight = group sortedRight
   
-  --print groupedRight
-  --print groupedLeft
   let groupedRightPair = [(maximum a, a) | a <- groupedRight]
   let groupedLeftPair = [(maximum a, a) | a <- groupedLeft]
   let simScore = sum [(maximum a) * count * (length a) | a <- groupedLeft, let count = numberOfElements a groupedRightPair]
@@ -41,13 +34,10 @@ main = do
   print simScore
   hClose handle
 
-
 numberOfElements :: [Int] -> [(Int, [Int])] -> Int
 numberOfElements a b = length (case lookup (maximum a) b of
                                  Just xs -> xs
-                                 Nothing -> []
-                              )
-  
+                                 Nothing -> [])
 
 toPair :: [String] -> (Int, Int)
 toPair [x, y] = (read x, read y)
